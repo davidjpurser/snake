6450f517bf0700b56b1249f1c860d08ce80d7ca7
David Purser <davidjpurser@gmail.com>
2015-06-19 23:24:53 +0100

    add some styles

--- j.js
+++ j.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function(){
 
-
+  //constants
   var N = 1;
   var E = 2;
   var S = 3;
@@ -12,10 +12,15 @@ $(document).ready(function(){
   var H = 5;
   var F = 6;
 
+  //coordinates of the head and tail of the snake
   var last;
   var head;
-  var direction;
+  //timer
   var timer;
+  //Last commanded direction
+  var direction;
+  //directions to use before falling back to last command
+  var directionStack;
 
   var width = 10;
   var height = 10;
@@ -25,12 +30,11 @@ $(document).ready(function(){
   var spacesInPlay = 0;
 
 
-  function get(x,y){
-    return {
-      x : x,
-      y : y
-    };
-  }
+  
+
+  /******************/
+  /* Render & Setup */
+  /******************/
 
 
   function start(){
@@ -46,74 +50,70 @@ $(document).ready(function(){
 
     game = [];
     for (var i = 0; i < height; i++){
-      game[i] = [];
       var tr = $('<tr/>');
       table.append(tr);
       for (var j = 0; j < width; j++){
-
-        var td = $('<td>');
-        tr.append(td);
-        td.data('row', i);
-        td.data('column', j);
-        game[i][j] = {
-          state: 0,
-          td: td,
-          food: false
-
-        };
-        updateCoord(get(i,j));
+        gs = defaultGameState(i, j);
+        tr.append(gs.td)
       }
     }
     last = get(0,0);
     head = get(0,0);
     setGameState(head, H);
-    updateCoord(head);
     direction = E;
+    directionStack = [];
     timer = setInterval(function() {
       next();
     }, 300);
   }
 
-  function getCoodsFromTd(td){
-    return get(td.data('row'), td.data('column'));
+  function endGame(){
+    clearInterval(timer);
+    $('#gameover').html("GAME OVER!");
   }
 
-  function updateCoord(coord){
+  function rerender(coord){
     var td = game[coord.x][coord.y].td;
     var state = gameState(coord);
+
+    td.removeClass("snake");
+    td.removeClass("empty");
     switch(state) {
         case N:
-            td.html("N");
+            td.addClass("snake");
             break;
         case E:
-            td.html("E");
+            td.addClass("snake");
             break;
         case S:
-            td.html("S");
+            td.addClass("snake");
             break;
         case W:
-            td.html("W");
+            td.addClass("snake");
             break;
         case H:
-            td.html("H");
+            td.addClass("snake");
+            td.addClass("head");
             break;
         default:
-            td.html("O");
+            td.addClass("empty");
     }
 
     if (isFood(coord)) {
-      td.append("<sub>F</sub>");
+      td.addClass("food");
+    }else {
+      td.removeClass("food");
     }
 
   }
 
-  function isFood(coord) {
-    return game[coord.x][coord.y].food;
-  }
 
-  function mod(n, m) {
-        return ((n % m) + m) % m;
-  }
+
+
+  /**************/
+  /* Game State */
+  /**************/
+ 
 
   function getInDir(coords, dir) {
     coord = dupli(coords);
@@ -135,9 +135,24 @@ $(document).ready(function(){
     return coord;
   }
 
-  function dupli(coord){
-    return get(coord.x, coord.y);
+  function defaultGameState(i, j) {
+
+    if (!$.isArray(game)) {
+      game = [];
+    }
+    if (!$.isArray(game[i])){
+      game[i] = [];
+    }
+
+    game[i][j] = {
+      state: O,
+      td: $('<td>'),
+      food: false
+    };
+    rerender(get(i,j))
+    return game[i][j];
   }
+
 
   function gameState(coord) {
     return game[coord.x][coord.y].state;
@@ -145,6 +160,7 @@ $(document).ready(function(){
 
   function setGameState(coord, state) {
     game[coord.x][coord.y].state = state;
+    rerender(coord);
   }
 
   function next() {
@@ -153,52 +169,87 @@ $(document).ready(function(){
       addFood();
     }
 
-    var shouldBeNext = getInDir(head, direction);
-
-    if (isFood(last)) {
-      removeFood(last);
-      updateCoord(last);
-      spacesInPlay--;
+    var dir;
+    if (directionStack.length == 0) {
+      dir = direction;
     } else {
-      var directionLast = gameState(last);
-      if (directionLast == H) {
-        console.log("is head");
-        directionLast = direction;
-      }
-      var newLast = getInDir(last, directionLast);
-      setGameState(last, O);
-      updateCoord(last);
-      last = newLast;
+      dir = directionStack.shift();
     }
 
-    if (gameState(shouldBeNext) != O) {
-      endGame();
-      return;
+    var shouldBeNext = getInDir(head, dir);
+
+    var nextHeadGameState = gameState(shouldBeNext);
+    // Fail if there is a (head taking anything other than the last) || a swap of head and tail
+    if ( (nextHeadGameState != O &&  !coordEquals(shouldBeNext, last)) || coordEquals(getInDir(shouldBeNext, nextHeadGameState), head)) {
+        endGame();
+        return;
     }
+
+    //Go ahead guarentee
 
     if (isFood(shouldBeNext)) {
       foodInPlay--;
     }
 
-    //gameState == 0 means that it head == last as the last management has moved the last forward once already (to the new shouldBeNext).
-    if (gameState(last) != O) {
-      setGameState(head, direction);
-      updateCoord(head);
+    //assume just head
+    var justHead = false;
+    
+    var directionLast = gameState(last);
+    //check if it just the head
+    if (directionLast == H) {
+      justHead = true;
+      directionLast = dir;
     }
+
+    var didEat = false;
+    if (isFood(last)) {
+      didEat = true;
+      removeFood(last);
+      spacesInPlay--;
+    } else {
+     
+      var newLast = getInDir(last, directionLast);
+      
+      setGameState(last, O);
+      last =  newLast;
+    }
+
+    if (!justHead || didEat) {
+      setGameState(head, dir);
+    } 
+
     head = shouldBeNext;
     setGameState(head, H);
-    updateCoord(head);
 
     $('#food').html(foodInPlay);
     $('#spaces').html(spacesInPlay);
     
   }
 
-  function endGame(){
-    clearInterval(timer);
-    alert('die');
+
+  /*********/
+  /* COORD */
+  /*********/
+
+  function coordEquals(a, b) {
+    return a.x == b.x && a.y == b.y;
   }
 
+  function get(x,y){
+    return {
+      x : x,
+      y : y
+    };
+  }
+
+  function dupli(coord){
+    return get(coord.x, coord.y);
+  }
+
+
+  /********/
+  /* FOOD */
+  /********/
   function addFood(){
     if (spacesInPlay > 0 && foodInPlay < 2){
       var coord;
@@ -210,35 +261,48 @@ $(document).ready(function(){
 
       putFood(coord)
       foodInPlay++;
-      updateCoord(coord);
     }
   }
-
-  function isSpaceForFood() {
-    for (var i = 0; i < width; i++) {
-      for (var j = 0; j < height; j++){
-        if (gameState(get(i,j)) == O) {
-          return true;
-        } 
-      }
-    }
-    return false;
+  function isFood(coord) {
+    return game[coord.x][coord.y].food;
   }
-
   function putFood(coord) {
     game[coord.x][coord.y].food = true;
+    rerender(coord);
   }
   function removeFood(coord) {
-     game[coord.x][coord.y].food = false;
+    game[coord.x][coord.y].food = false;
+    rerender(coord);
   }
+
+
+  /*********/
+  /* UTILS */
+  /*********/
 
   //get a number between 0 and max - 1 (max not included)
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
+  function mod(n, m) {
+        return ((n % m) + m) % m;
+  }
+
+  function format() {
+    var wwidth = $(window).width();
+    var wheight = $(window).height() - 30;
+    var min = Math.min(wwidth, wheight);
+
+    $('#game table').width(min).height(min);
+    $('#game table td').width(wwidth / width);
+  }
+
+  $(window).on('resize',function(){ format(); });
+
   $('#start').on('click', function(){
     start();
+    format();
   });
 
   $('#next').on('click', function(){
@@ -253,21 +317,24 @@ $(document).ready(function(){
   $('body').on('keydown', function(e){
     console.log(e);
     if (e.keyCode >=37 && e.keyCode <=40) {
-
+      var dir;
       switch(e.keyCode) { 
         case 37:
-            direction = W;
+            dir = W;
             break;
         case 38:
-            direction = N;
+            dir = N;
             break;
         case 39:            
-            direction = E;
+            dir = E;
             break;
         case 40:
-            direction = S;
+            dir = S;
             break;
       } 
+      directionStack.push(dir);
+      direction = dir;
+
     } else if(e.keyCode == 32){
         // addFood();
     }
